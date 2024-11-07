80bdbba770f61b53576cef491c100e2045fb061c
unknown <amirsyah87@gmail.com>
2024-11-07 15:35:43 +0800

    ok dah

--- alumoptimization.py
+++ alumoptimization.py
@@ -1,6 +1,31 @@
+import pandas as pd
+
+file_path = r'C:\Users\iconf\OneDrive\Documents\VS Code\alumoptimization\Alumopt.xlsx'  # Replace with your actual file path
+
+df = pd.read_excel(file_path, usecols="A:D", header=None, names=['Column A', 'Column B', 'Column C', 'Column D'], engine='openpyxl')
+
+#print(f"DataFrame Shape: {df.shape}")
+#print(df.head()) 
+
+#try:
+#    print("Value in 4th column (index 3):", int(df.iloc[0, 3]))  # Access the 4th column of first row
+#except IndexError:
+#    print("IndexError: The DataFrame doesn't have enough columns.")
+
+df['Column A'] = pd.to_numeric(df['Column A'], errors='coerce')  # Handle errors gracefully
+df['Column B'] = pd.to_numeric(df['Column B'], errors='coerce')
+
+#print(int(df.iloc[0, 3]))
+
+ext = []
+for index, row in df.iterrows():
+    ext.extend([int(row['Column A'])] * int(row['Column B']))
+#print(ext)
+#print(int(df.iloc[0, 3]))
+
 ext = [5600, 5600, 5600, 1200, 1200, 400, 400, 2500, 2500, 2500, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000]
 cutLi = []
-rawMate = 5600
+rawMate = int(df.iloc[0, 3])
 
 
 for i in range(len(ext)):  # nak loop ext
